989c141795cb3bf1984e22c2b479f55a499e20ec
Alejandro Gonzalez Osorio <ale96glz@gmail.com>
2024-10-27 21:23:10 -0600

    Cambios en el search.py

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
+# 
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,7 +18,6 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
-
 
 class SearchProblem:
     """
@@ -69,18 +68,16 @@ def tinyMazeSearch(problem):
     sequence of moves will be incorrect, so only use this for tinyMaze.
     """
     from game import Directions
-
     s = Directions.SOUTH
     w = Directions.WEST
-    return [s, s, w, s, w, w, s, w]
-
+    return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
     pilaNodos = util.Stack()
     visitados = set()
     estadoInicial = problem.getStartState()
     pilaNodos.push((estadoInicial, []))
-    visitados.add(estadoInicial)
+    visitados.add(estadoInicial) 
 
     while not pilaNodos.isEmpty():
         estadoActual, acciones = pilaNodos.pop()
@@ -90,7 +87,7 @@ def depthFirstSearch(problem):
 
         for proximoNodo, accion, costo in problem.getSuccessors(estadoActual):
             if proximoNodo not in visitados:
-                visitados.add(proximoNodo)
+                visitados.add(proximoNodo) 
                 nuevaAccion = acciones + [accion]
                 pilaNodos.push((proximoNodo, nuevaAccion))
 
@@ -102,7 +99,7 @@ def breadthFirstSearch(problem):
     visitados = set()
     estadoInicial = problem.getStartState()
     colaNodos.push((estadoInicial, []))
-    visitados.add(estadoInicial)
+    visitados.add(estadoInicial) 
 
     while not colaNodos.isEmpty():
         estadoActual, acciones = colaNodos.pop()
@@ -112,11 +109,13 @@ def breadthFirstSearch(problem):
 
         for proximoNodo, accion, costo in problem.getSuccessors(estadoActual):
             if proximoNodo not in visitados:
-                visitados.add(proximoNodo)
+                visitados.add(proximoNodo) 
                 nuevaAccion = acciones + [accion]
                 colaNodos.push((proximoNodo, nuevaAccion))
 
     return []
+
+
 
 
 def uniformCostSearch(problem):
@@ -124,6 +123,7 @@ def uniformCostSearch(problem):
     visitados = set()
     estadoInicial = problem.getStartState()
     colaPrioridad.push((estadoInicial, [], 0), 0)
+    
 
     while not colaPrioridad.isEmpty():
         estadoActual, acciones, costoActual = colaPrioridad.pop()
@@ -134,12 +134,10 @@ def uniformCostSearch(problem):
             visitados.add(estadoActual)
             for proximoNodo, accion, costo in problem.getSuccessors(estadoActual):
                 if proximoNodo not in visitados:
-                    nuevoCosto = costoActual + costo
+                    nuevoCosto = costoActual + costo 
                     nuevaAccion = acciones + [accion]
-                    colaPrioridad.push(
-                        (proximoNodo, nuevaAccion, nuevoCosto), nuevoCosto
-                    )
-
+                    colaPrioridad.push((proximoNodo, nuevaAccion, nuevoCosto), nuevoCosto)
+                    
     return []
 
 
@@ -150,11 +148,26 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
-
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    colaPrioridad = util.PriorityQueue()
+    visitados = {}
+    estado_inicial = problem.getStartState()
+    colaPrioridad.push((estado_inicial, []), heuristic(estado_inicial, problem))
+    visitados[estado_inicial] = 0
+
+    while not colaPrioridad.isEmpty():
+        estadoActual, acciones = colaPrioridad.pop()
+
+        if problem.isGoalState(estadoActual):
+            return acciones
+
+        for sucesor, accion, costo in problem.getSuccessors(estadoActual):
+            nuevo_costo = visitados[estadoActual] + costo
+            if sucesor not in visitados or nuevo_costo < visitados[sucesor]:
+                visitados[sucesor] = nuevo_costo
+                prioridad = nuevo_costo + heuristic(sucesor, problem)
+                colaPrioridad.push((sucesor, acciones + [accion]), prioridad)
+    return []
 
 
 # Abbreviations
